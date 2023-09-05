1f93173a64108c34b9b09d02d1f7afc39efaf01c
SadiqNahmetov <sadigfn@code.edu.az>
2023-09-05 20:12:59 +0400

    Created product page cart

--- furniture/src/pages/product/Product.jsx
+++ furniture/src/pages/product/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import changeTitle from "../../utils/Change";
 import Breadcrumbs from "../../components/Breadcrumbs";
-
+import ProductList from "../../components/ProductList";
 
 function Product() {
   useEffect(() => {
@@ -10,6 +10,7 @@ function Product() {
   return (
     <main>
       <Breadcrumbs />
+      <ProductList />
     </main>
   );
 }
